9870ec7d76da2e9ae5ca4ceff31b3920cb747c4a
Marcin Szuster <szust.marcin@gmail.com>
2020-09-24 00:08:31 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    hot: true
+  }
+};
