4935b5d469bf1415fbeabb8167f2e5bbff9cef0b
Marcin Szuster <szust.marcin@gmail.com>
2020-09-30 22:17:24 +0200

    Add queries for fetching planets

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,37 @@ export const GET_FILMS_LIST = gql`
   query GetFilmsList {
     allFilms {
       films {
+        id
         title
+      }
+    }
+  }
+`;
+
+export const GET_PLANETS_FROM_FILM = gql`
+  query GetPlanetsFromFilm($filmId: ID!) {
+    film(id: $filmId) {
+      planetConnection {
+        planets {
+          id
+          name
+          rotationPeriod
+          orbitalPeriod
+          diameter
+          climates
+          surfaceWater
+          population
+        }
+      }
+    }
+  }
+`;
+
+export const GET_PLANETS_LIST = gql`
+  query GetPlanetsList {
+    allPlanets {
+      planets {
+        name
       }
     }
   }
