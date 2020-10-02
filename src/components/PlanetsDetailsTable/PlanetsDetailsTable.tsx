f2d452f26aa50fb76b5e0f02355c3f61392b4de8
Marcin Szuster <szust.marcin@gmail.com>
2020-10-02 06:02:16 +0200

    Make table semantic valid

--- src/components/PlanetsDetailsTable/PlanetsDetailsTable.tsx
+++ src/components/PlanetsDetailsTable/PlanetsDetailsTable.tsx
@@ -9,30 +9,34 @@ type PlanetsDetailsTableProps = {
 const PlanetsDetailsTable: React.FC<PlanetsDetailsTableProps> = ({planets}) => {
   return (
     <Table>
-      <HeadRow>
-        <Cell marked>Name</Cell>
-        <Cell>Rotation period</Cell>
-        <Cell>Orbital period</Cell>
-        <Cell>Diameter</Cell>
-        <Cell>Climate</Cell>
-        <Cell>Surface water</Cell>
-        <Cell>Population</Cell>
-      </HeadRow>
-      {planets.map(planet => {
-        if (!planet) return;
+      <thead>
+        <HeadRow>
+          <Cell marked>Name</Cell>
+          <Cell>Rotation period</Cell>
+          <Cell>Orbital period</Cell>
+          <Cell>Diameter</Cell>
+          <Cell>Climate</Cell>
+          <Cell>Surface water</Cell>
+          <Cell>Population</Cell>
+        </HeadRow>
+      </thead>
+      <tbody>
+        {planets.map(planet => {
+          if (!planet) return;
 
-        return (
-          <tr key={planet.id}>
-            <Cell marked>{planet.name}</Cell>
-            <Cell>{planet.rotationPeriod}</Cell>
-            <Cell>{planet.orbitalPeriod}</Cell>
-            <Cell>{planet.diameter}</Cell>
-            <Cell>{planet.climates}</Cell>
-            <Cell>{planet.surfaceWater}</Cell>
-            <Cell>{planet.population}</Cell>
-          </tr>
-        );
-      })}
+          return (
+            <tr key={planet.id}>
+              <Cell marked>{planet.name}</Cell>
+              <Cell>{planet.rotationPeriod}</Cell>
+              <Cell>{planet.orbitalPeriod}</Cell>
+              <Cell>{planet.diameter}</Cell>
+              <Cell>{planet.climates}</Cell>
+              <Cell>{planet.surfaceWater}</Cell>
+              <Cell>{planet.population}</Cell>
+            </tr>
+          );
+        })}
+      </tbody>
     </Table>
   );
 };
